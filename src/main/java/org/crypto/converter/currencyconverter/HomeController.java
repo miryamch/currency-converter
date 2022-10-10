7b4a06b453ede6916bbca14a179305caf3a7c634
Miryam Chaabouni <miryam.chaabouni@gmail.com>
2022-10-10 22:52:37 +0200

    Dummy submit form

--- src/main/java/org/crypto/converter/currencyconverter/HomeController.java
+++ src/main/java/org/crypto/converter/currencyconverter/HomeController.java
@@ -1,18 +1,32 @@
 package org.crypto.converter.currencyconverter;
 
 import org.crypto.converter.currencyconverter.domain.CryptoCurrency;
+import org.crypto.converter.currencyconverter.model.CurrencyConverterModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.EnumSet;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("cryptocurrencies", EnumSet.allOf(CryptoCurrency.class));
+        model.addAttribute("currencyConverterModel", new CurrencyConverterModel());
+        model.addAttribute("currentUnitPrice", "???");
+        return "home/index";
+    }
+
+    @PostMapping("/convertCurrency")
+    public String convertCurrency(Model model, @ModelAttribute @Valid CurrencyConverterModel currencyConverterModel, BindingResult bindingResult) {
+        model.addAttribute("cryptocurrencies", EnumSet.allOf(CryptoCurrency.class));
+        model.addAttribute("currencyConverterModel", currencyConverterModel);
+        model.addAttribute("currentUnitPrice", "6,99");
         return "home/index";
     }
 
---
+++ src/main/java/org/crypto/converter/currencyconverter/model/CurrencyConverterModel.java
@@ -0,0 +1,16 @@
+package org.crypto.converter.currencyconverter.model;
+
+import lombok.Data;
+import org.crypto.converter.currencyconverter.domain.CryptoCurrency;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class CurrencyConverterModel {
+
+    @NotNull
+    private CryptoCurrency cryptoCurrency;
+
+    //@Pattern(regexp = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+    private Integer ipAddress;
+}
